a9c34ed10951d60593fcfacc90c5485a28995138
vandyand <vandyand@gmail.com>
2023-07-19 23:48:55 -0400

    checkpoint - idontknow/pit+.clj working. currently abstracting this to do enhance files in addition to prompts

--- idontknow/pit+.clj
+++ idontknow/pit+.clj
@@ -1,6 +1,7 @@
-(ns mimi.bible_app.pit+
+(ns mimi.idontknow.pit+
   (:require [clj-http.client :as http]
-            [clojure.data.json :as json]))
+            [clojure.data.json :as json]
+            [clojure.core.async :as async :refer [<! >! go chan]]))
 
 (defn parse-response-body [response]
   (json/read-str (response :body) :key-fn keyword))
@@ -12,7 +13,7 @@
         body {:model "gpt-3.5-turbo"
               :messages [{:role "user" :content query}]
               :temperature 1.7
-              :max_tokens 256 }
+              :max_tokens 1024 }
         json-body (json/write-str body)
         byte-array-body (.getBytes json-body "UTF-8")]
     (http/post url {:body byte-array-body
@@ -36,12 +37,25 @@
     ;;   p+
       ))
 
+;;;;;;;;;;;;;;;;;;;; DEFINE OPERABLES ;;;;;;;;;;;;;;;
+(defn get-operable [task, test]
+  {:task task :test test}) ; mode is like "compare two" or "test one"
+  ;{:task "that which we are to do" :test "that by which we steer/direct (think 'which summary is better?')"})
+  ;{:task "Summarize Biblical Texts" :test "which of these options evidences the ability to X more adequately? (where X is the task)"})
+
+(def bible-operable (get-operable "Summarize (the provided) Biblical Texts" "Which summary is more interesting and intriguing?"))
+
+(def file-x-operable (get-operable "Make a file that does X" {:mode "compare two" :question "Which file does X better?"}))
+
+(def file-bash-operable (get-operable "Make a bash script that echoes 'Hello World!'" "Which bash script echoes 'Hello World!' most adequately?"))
+
+
 ;;;;;;;;;;;;;;;;;;;; GENERATE AGENTS ;;;;;;;;;;;;;
 
-(def task-desc "Summarize Biblical Texts")
+;; (def task-desc "Summarize Biblical Texts")
 
 (defn gen-agent-body []
-  (query-gpt (str "Please generate an agent prompt for me. The agent's mission will be to " task-desc ". No ai voice. No ai description. No ai summary.")))
+  (query-gpt (str "Please generate an agent prompt for me. The agent's mission will be to " (:task file-bash-operable) ". No ai voice. No ai description. No ai summary.")))
 
 (defn gen-agent
   ([] (gen-agent (gen-agent-body)))
@@ -58,14 +72,14 @@
          (gen-agent-body))))
 
 (defn mutate-agent [agent]
-  (gen-agent (query-gpt (str "Please improve this prompt so that it is better able to " task-desc ": "
+  (gen-agent (query-gpt (str "Please improve this agent prompt so that it is better able to " (:task file-bash-operable) ": "
                              (:body agent)
                              "No ai voice. No ai description. No ai summary."))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;; PERFORM ACTION - CREATE TRIAL DATA ;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn perform [task-info agent]
-  (query-gpt (str "Your mission is to: " task-desc ". Here is the info: " task-info ". " (:body agent))))
+  (query-gpt (str "Your mission is to: " (:task file-bash-operable) ". Here is the info: " task-info ". " (:body agent))))
 
 #_(summarize (get-chapter "genesis" 1) (gen-agent))
 
@@ -107,47 +121,50 @@
              {:error "Reached the end of the Bible"})))))))
 
 
-(defn battle [task-info summary1 summary2]
+(defn battle [meat op1 op2]
   (query-gpt (str
-              "which of these two summaries do you find most insightful and interesting?"
-              " here is the summary verse: "
-              task-info
-              " and here are the summaries: "
-              "SUMMARY1: " summary1 "\n"
-              "SUMMARY2: " summary2 "\n"
-              "No ai voice. No ai description. No ai summary. No ai rationale. Please respond ONLY with 'SUMMARY1' or 'SUMMARY2'")))
+              (:test file-bash-operable)
+              meat 
+              "OPTION1: " op1 "\n"
+              "OPTION2: " op2 "\n"
+              "No ai voice. No ai description. No ai summary. No ai rationale. Please respond ONLY with 'OPTION1' or 'OPTION2'")))
 
-(let [agents (gen-agents (gen-agent-bodies 5))
-      foo (spit "agents.edn" agents)
-      foo (p+ agents)
-      first-ref {:book "genesis" :chapter 1 :verse 1}]
-  (loop [i 0 ref first-ref agents agents]
-    (let [foo (println "--------------- CHALLENGE #" i ", verse: " ref " -------------------------------")
-          shuf-agents (shuffle agents)
-          agent1 (first shuf-agents) ;; later - enforce unique agents
-          agent2 (second shuf-agents)
-          task-info (get-task-info ref)
-          foo (p+ {:agent1 agent1})
-          foo (p+ {:agent2 agent2})
-          summary1 (perform task-info agent1)
-          foo (p+ {:summary1 summary1})
-          summary2 (perform task-info agent2)
-          foo (p+ {:summary2 summary2})
-          who-won? (battle task-info summary1 summary2)
-          winner (if (clojure.string/includes? who-won? "SUMMARY1") agent1 agent2)
-          loser (if (clojure.string/includes? who-won? "SUMMARY1") agent2 agent1)
-          foo (p+ {:who-won? who-won? :id (:id winner)})
-          child (mutate-agent winner)
-          foo (spit "agents.edn" child :append true)
+(let [agents (gen-agents (gen-agent-bodies 3))
+        foo (spit "agents.edn" agents)
+        foo (p+ agents)
+        first-ref {:book "genesis" :chapter 1 :verse 1}]
+    (loop [i 0 ref first-ref agents agents]
+      (let [foo (println "--------------- CHALLENGE #" i ", verse: " ref " -------------------------------")
+            shuf-agents (shuffle agents)
+            agent1 (first shuf-agents) ;; later - enforce unique agents
+            agent2 (second shuf-agents)
+            task-info (get-task-info ref)
+            foo (p+ {:agent1 agent1})
+            foo (p+ {:agent2 agent2})
+            summary1 (perform task-info agent1)
+            foo (p+ {:summary1 summary1})
+            summary2 (perform task-info agent2)
+            foo (p+ {:summary2 summary2})
+            who-won? (battle task-info summary1 summary2)
+            winner (if (clojure.string/includes? who-won? "OPTION1") agent1 agent2)
+            loser (if (clojure.string/includes? who-won? "OPTION1") agent2 agent1)
+            foo (p+ {:who-won? who-won? :id (:id winner)})
+            child (mutate-agent winner)
+            foo (spit "agents.edn" child :append true)
         ;;   foo (p+ {:child child})
-          new-agents+ (vec (conj agents child))
-          new-agents (vec (remove #(= (:id %) (:id loser)) new-agents+))
+            new-agents+ (vec (conj agents child))
+            new-agents (vec (remove #(= (:id %) (:id loser)) new-agents+))
           ;; foo (p+ {:new-agents new-agents})
         ;;   foo (println {:new-agents new-agents})
         ;;   foo (p+ {:len-new-agents (count new-agents)})
-          next-verse-ref (get-next-task-info ref)
+            next-verse-ref (get-next-task-info ref)
         ;;   foo (p+ {:next-verse-ref next-verse-ref})
-          ]
-      (if (or (> i 10000) (contains? next-verse-ref :error))
-        new-agents
-        (recur (inc i) next-verse-ref new-agents)))))
+            ]
+        (if (or (> i 10000) (contains? next-verse-ref :error))
+          new-agents
+          (recur (inc i) next-verse-ref new-agents)))))
+
+
+
+
+
