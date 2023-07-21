2178646e2a45ebc132b381f4bb7cc17df45f2bfa
vandyand <vandyand@gmail.com>
2023-07-21 01:00:31 -0400

    checkpoint - 1:00

--- summerizer/core-1-1.clj
+++ summerizer/core-1-1.clj
@@ -2,7 +2,8 @@
   (:require [clj-http.client :as http]
             [clojure.data.json :as json]
             [clojure.java.io :as io]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.walk :as walk]))
 
 (defn parse-response-body [response]
   (json/read-str (:body response) :key-fn keyword))
@@ -40,6 +41,7 @@
                    Please update this file with enhancements without increasing the file size.
                    Add new enhancements as clojure code allowing this file greater utility.
                    If you need more room, please refactor existing code in order to make room.
+                   Also, please remove any dead, unused code.
                    Here is the file content: " file-content)))
 
 (defn process-file [file]
@@ -52,5 +54,32 @@
   (let [files+dirs (file-seq (clojure.java.io/file target-dir-path))
         files (filter #(.isFile %) files+dirs)]
     (mapv process-file files)))
+
+(defn update-file [file-path enhancement]
+  (let [content (slurp file-path)
+        updated-content (str content enhancement)]
+    (spit file-path updated-content)))
+
+(defn get-file-size [file-path]
+  (let [file (io/file file-path)]
+    (:file-size (io/file file))))
+
+(defn get-file-lines [file-path]
+  (let [file (io/file file-path)]
+    (with-open [reader (io/reader file)]
+      (doall (line-seq reader)))))
+
+(defn append-to-file [file-path content]
+  (with-open [writer (io/writer file-path :append true)]
+    (.write writer content)))
+
+(defn prepend-to-file [file-path content]
+  (let [existing-content (slurp file-path)]
+    (spit file-path (str content existing-content))))
+
+(defn insert-at-line [file-path line content]
+  (let [lines (get-file-lines file-path)
+        updated-lines (update lines line #(str % content "\n"))]
+    (spit file-path (clojure.string/join "\n" updated-lines))))
 
 (process-dir "/Users/kingjames/personal/mimi/summerizer/core-1-1.clj")
