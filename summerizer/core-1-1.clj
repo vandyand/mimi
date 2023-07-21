4eb070c9a5d0cc6640764376e53052234388baf7
vandyand <vandyand@gmail.com>
2023-07-21 01:39:33 -0400

    checkpoint - 1:39

--- summerizer/core-1-1.clj
+++ summerizer/core-1-1.clj
@@ -4,76 +4,55 @@
             [clojure.java.io :as io]
             [clojure.string :as str]))
 
-(defn parse-response-body [response]
+;; Parsing HTTP response
+(defn parse-http-response [response]
   (json/read-str (:body response) :key-fn keyword))
 
-(defn send-query-to-gpt [query]
+;; Sending a query to the GPT service
+(defn send-query-to-service [query]
   (let [url "https://api.openai.com/v1/chat/completions"
         headers {"Content-Type" "application/json"
                  "Authorization" (str "Bearer " (System/getenv "OPENAI_API_KEY"))}
-        body {:model "gpt-3.5-turbo"
+        body {:model "gpt-4"
               :messages [{:role "user" :content query}]
-              :temperature 1.2}
+              :temperature 1.25}
         json-body (json/write-str body)]
-    (http/post url {:body (.getBytes json-body "UTF-8")
-                    :headers headers})))
+     (http/post url {:body (.getBytes json-body "UTF-8")
+                     :headers headers})))
 
-(defn get-gpt-response [query]
+;; Retrieving service response
+(defn retrieve-service-response [query]
   (-> query
-      send-query-to-gpt
-      parse-response-body
+      send-query-to-service
+      parse-http-response
       :choices
       first
       :message
       :content))
 
-(defn enhance-file [file-content]
-  (let [prompt (str "Please consider the following updates and enhancements for this file:\n"
-                    "- Refactor existing code to remove dead, unused code.\n"
-                    "- Rename functions and variables for clarity and better naming conventions.\n"
-                    "- Allow for additional enhancements as Clojure code to improve utility.\n"
-                    "- Update the string prompt to provide more context and details for the desired enhancements.\n\n"
-                    "Remember to return ONLY the updated file content. Avoid AI voices, summaries, and descriptions.\n\n"
-                    "Here is the file content:\n\n"
-                    file-content)
-        enhanced-content (get-gpt-response prompt)]
+;; Enhancing a given file content
+(defn enhance-file-content [content]
+  (let [request (str "Please return following enhanced file content based on these actions:\n\n"
+                      "1. Remove any broken or unused code blocks.\n"
+                      "2. Comply the naming conventions such as function names or variable identifiers.\n"
+                      "3. Next, consider codes that further boost the functionality of this file.\n"
+                      "4. Lastly, expand this given transcript for a fuller understanding of the requested enhancement.\n\n"
+                      "No repeating writings, introductions, and conclusions are necessary.\n\n"
+                      "-- File Content: --\n\n" content)
+        enhanced-content (retrieve-service-response request)]
     enhanced-content))
 
-(defn process-file [file]
-  (let [content (slurp file)
-        summary (enhance-file content)]
-    (spit file summary)
-    (println (str "Done processing " (.getName file)))))
+;; Process file with the above enhancements
+(defn process-and-enhance-file [file]
+  (let [raw-content (slurp file)
+        enhanced-content (enhance-file-content raw-content)]
+    (spit file enhanced-content)
+    (println (str "Finished enhancing file: " (.getName file)))))
 
-(defn process-dir [target-dir-path]
-  (let [files (filter #(.isFile %) (file-seq (clojure.java.io/file target-dir-path)))]
+;; Seamlessly processing all files within a specified directory
+(defn process-all-files-in-dir [directory-path]
+  (let [files (filter #(.isFile %) (file-seq (io/file directory-path)))]
     (doseq [file files]
-      (process-file file))))
+        (process-and-enhance-file file))))
 
-(defn update-file [file-path enhancement]
-  (let [content (slurp file-path)
-        updated-content (str content enhancement)]
-    (spit file-path updated-content)))
-
-(defn get-file-size [file-path]
-  (-> (io/file file-path) :file-size))
-
-(defn get-file-lines [file-path]
-  (let [file (io/file file-path)]
-    (with-open [reader (io/reader file)]
-      (doall (line-seq reader)))))
-
-(defn append-to-file [file-path content]
-  (with-open [writer (io/writer file-path :append true)]
-    (.write writer content)))
-
-(defn prepend-to-file [file-path content]
-  (let [existing-content (slurp file-path)]
-    (spit file-path (str content existing-content))))
-
-(defn insert-at-line [file-path line content]
-  (let [lines (get-file-lines file-path)
-        updated-lines (update lines line #(str % content "\n"))]
-    (spit file-path (clojure.string/join "\n" updated-lines))))
-
-(process-dir "/Users/kingjames/personal/mimi/summerizer/core-1-1.clj")
+(process-all-files-in-dir "/Users/kingjames/personal/mimi/summerizer/core-1-1.clj")
