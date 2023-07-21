6ad3df7765feec18f3d148b9e99e3d2549da2226
vandyand <vandyand@gmail.com>
2023-07-21 02:02:30 -0400

    checkpoint - 2:02 - end for now. not bad.

--- summerizer/core-1-1.clj
+++ summerizer/core-1-1.clj
@@ -4,22 +4,25 @@
             [clojure.java.io :as java-io]
             [clojure.string :as str]))
 
+;; Function to parse API response
 (defn parse-api-response [response-body]
   (json/read-str (:body response-body) :key-fn keyword))
 
-(defn fetch-api-response [api-request]
-  (let [aggregated-api-endpoint "https://api.openai.com/v1/chat/completions"
-        request-configuration {:model "gpt-4"
-                               :messages [{:role "user" :content api-request}]
-                               :temperature 0.75}
+;; Function to fetch API response
+(defn fetch-api-response [user-request]
+  (let [api-endpoint "https://api.openai.com/v1/chat/completions"
+        request-config {:model "gpt-4"
+                        :messages [{:role "user" :content user-request}]
+                        :temperature 0.75}
         header-content {"Content-Type" "application/json"
                         "Authorization" (str "Bearer " (System/getenv "OPENAI_API_KEY"))}
-        formatted-request (json/write-str request-configuration)]
-    (http-client/post aggregated-api-endpoint {:body (.getBytes formatted-request "UTF-8")
-                                               :headers header-content})))
+        formatted-request (json/write-str request-config)]
+    (http-client/post api-endpoint {:body (.getBytes formatted-request "UTF-8")
+                                    :headers header-content})))
 
-(defn extract-output [api-request]
-  (-> api-request
+;; Function to extract output from API response
+(defn extract-output [user-request]
+  (-> user-request
       fetch-api-response
       parse-api-response
       :choices
@@ -27,24 +30,26 @@
       :message
       :content))
 
+;; Function to apply enhancements to the file content
 (defn apply-enhancements [file-content]
-  (let [enhancement-description (str "Perform the following enhancements on a given block of code:"
-                                     "1. Remove unfitting or inoperitave blocks of code."
-                                     "2. Align the output with accepted naming standards for both functions and variable identifiers:"
-                                     "3. Review any instances in the code that may delay the execution time of this file."
-                                     "4. Evaluate and improve the existing documentation for a better understanding of enhancement guidelines."
-                                     "5. Create functional framework for incorporating enhancements into this file."
-                                     "Avoid introducing any elements that could double uncertainties or disrupt the file's overall functionality. Ensure that all new updates made to this file will make external contribution API's increasingly comprehended and edited."
-                                     "-- Provided File: --" file-content)
-        enhanced-file-content (extract-output enhancement-description)]
-    enhanced-file-content))
+  (let [enhancement-desc (str "Enhancements:"
+                              "1. Remove unfitting blocks of code."
+                              "2. Align with naming standards."
+                              "3. Review possible delays."
+                              "4. Improve documentation."
+                              "5. Create functional framework for enhancements."
+                              "-- File: --" file-content)
+        enhanced-content (extract-output enhancement-desc)]
+    enhanced-content))
 
+;; Function to refine source code
 (defn refine-code [source-file]
   (let [file-source (slurp source-file)
         optimized-content (apply-enhancements file-source)]
     (spit source-file optimized-content)
     (println (str "File enhancement successful: " (.getName source-file)))))
 
+;; Function to initiate enhancement process
 (defn initiate-enhancement-process [file-path]
   (refine-code file-path))
 
